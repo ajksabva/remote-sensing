b588c1b2d2788f7f61cf9766fa752575ce16ab08
tsdsnk <903257695@qq.com>
2024-10-22 16:51:21 +0800

    initcreate new branch: multi-scenario copy std file

---
+++ multi-scenario/configs/rotated_imted_oriented_rcnn_vit_base_3x_masativ2_rr_le90_stdc_xyawh321v.py
@@ -0,0 +1,329 @@
+dataset_type = 'MASATIv2Dataset'
+data_root = '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RResize', img_scale=(800, 800)),
+    dict(
+        type='RRandomFlip',
+        flip_ratio=[0.25, 0.25, 0.25],
+        direction=['horizontal', 'vertical', 'diagonal'],
+        version='le90'),
+    dict(
+        type='PolyRandomRotate',
+        rotate_ratio=0.5,
+        angles_range=180,
+        auto_bound=False,
+        version='le90'),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=True),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(800, 800),
+        flip=False,
+        transforms=[
+            dict(type='RResize'),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=32,
+    train=dict(
+        type='MASATIv2Dataset',
+        ann_file=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/ImageSets/trainval.txt',
+        ann_subdir=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/FullDataSet/Annotations/',
+        img_subdir=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/FullDataSet/AllImages/',
+        img_prefix=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/FullDataSet/AllImages/',
+        filter_imgs=True,
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(type='RResize', img_scale=(800, 800)),
+            dict(
+                type='RRandomFlip',
+                flip_ratio=[0.25, 0.25, 0.25],
+                direction=['horizontal', 'vertical', 'diagonal'],
+                version='le90'),
+            dict(
+                type='PolyRandomRotate',
+                rotate_ratio=0.5,
+                angles_range=180,
+                auto_bound=False,
+                version='le90'),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+        ],
+        version='le90'),
+    val=dict(
+        type='MASATIv2Dataset',
+        ann_file=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/ImageSets/test.txt',
+        ann_subdir=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/FullDataSet/Annotations/',
+        img_subdir=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/FullDataSet/AllImages/',
+        img_prefix=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/FullDataSet/AllImages/',
+        filter_imgs=True,
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(800, 800),
+                flip=False,
+                transforms=[
+                    dict(type='RResize'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='DefaultFormatBundle'),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ],
+        version='le90'),
+    test=dict(
+        type='MASATIv2Dataset',
+        ann_file=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/ImageSets/test.txt',
+        ann_subdir=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/FullDataSet/Annotations/',
+        img_subdir=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/FullDataSet/AllImages/',
+        img_prefix=
+        '/root/yrz/Large-Selective-Kernel-Network/data/MASATI-v2-hrsc/FullDataSet/AllImages/',
+        filter_imgs=True,
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(800, 800),
+                flip=False,
+                transforms=[
+                    dict(type='RResize'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='Pad', size_divisor=32),
+                    dict(type='DefaultFormatBundle'),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ],
+        version='le90'))
+evaluation = dict(interval=3, metric='mAP')
+optimizer = dict(
+    type='AdamW',
+    lr=0.00025,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    constructor='LayerDecayOptimizerConstructor',
+    paramwise_cfg=dict(num_layers=12, layer_decay_rate=0.75))
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.3333333333333333,
+    step=[24, 33])
+runner = dict(type='EpochBasedRunner', max_epochs=108)
+checkpoint_config = dict(interval=1)
+log_config = dict(interval=50, hooks=[dict(type='TextLoggerHook')])
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+opencv_num_threads = 0
+mp_start_method = 'fork'
+pretrained = 'data/pretrained/mae_pretrain_vit_base_full.pth'
+angle_version = 'le90'
+norm_cfg = dict(type='LN', requires_grad=True)
+model = dict(
+    type='RotatedimTED',
+    proposals_dim=6,
+    backbone=dict(
+        type='VisionTransformer',
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='data/pretrained/mae_pretrain_vit_base_full.pth'),
+        img_size=224,
+        patch_size=16,
+        embed_dim=768,
+        depth=12,
+        num_heads=12,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        drop_path_rate=0.2,
+        learnable_pos_embed=True,
+        use_checkpoint=False,
+        with_simple_fpn=True,
+        last_feat=True),
+    neck=dict(
+        type='SimpleFPN',
+        in_channels=[768, 768, 768, 768],
+        out_channels=256,
+        norm_cfg=dict(type='LN', requires_grad=True),
+        use_residual=False,
+        num_outs=5),
+    rpn_head=dict(
+        type='OrientedRPNHead',
+        in_channels=256,
+        feat_channels=256,
+        version='le90',
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='MidpointOffsetCoder',
+            angle_range='le90',
+            target_means=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            target_stds=[1.0, 1.0, 1.0, 1.0, 0.5, 0.5]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(
+            type='SmoothL1Loss', beta=0.1111111111111111, loss_weight=1.0)),
+    roi_skip_fpn=False,
+    with_mfm=True,
+    roi_head=dict(
+        type='OrientedStandardRoIHeadimTED',
+        bbox_roi_extractor=[
+            dict(
+                type='RotatedSingleRoIExtractor',
+                roi_layer=dict(
+                    type='RoIAlignRotated',
+                    out_size=7,
+                    sample_num=2,
+                    clockwise=True),
+                out_channels=768,
+                featmap_strides=[4, 8, 16, 32]),
+            dict(
+                type='RotatedSingleRoIExtractor',
+                roi_layer=dict(
+                    type='RoIAlignRotated',
+                    out_size=7,
+                    sample_num=2,
+                    clockwise=True),
+                out_channels=768,
+                featmap_strides=[16])
+        ],
+        bbox_head=dict(
+            type='RotatedMAEBBoxHeadSTDC',
+            init_cfg=dict(
+                type='Pretrained',
+                checkpoint='data/pretrained/mae_pretrain_vit_base_full.pth'),
+            use_checkpoint=False,
+            in_channels=768,
+            img_size=224,
+            patch_size=16,
+            embed_dim=512,
+            depth=8,
+            num_heads=16,
+            mlp_ratio=4.0,
+            num_classes=1,
+            bbox_coder=dict(
+                type='DeltaXYWHAOBBoxCoder',
+                angle_range='le90',
+                norm_factor=None,
+                edge_swap=True,
+                proj_xy=True,
+                target_means=(0.0, 0.0, 0.0, 0.0, 0.0),
+                target_stds=(0.1, 0.1, 0.2, 0.2, 0.1)),
+            reg_class_agnostic=True,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0),
+            dc_mode_str_list=['', '', '', 'XY', '', 'A', '', 'WH'],
+            num_convs_list=[0, 0, 3, 3, 2, 2, 1, 1],
+            am_mode_str_list=['', '', 'V', 'V', 'V', 'V', 'V', 'V'],
+            rois_mode='rbbox')),
+    train_cfg=dict(
+        rpn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=0,
+            pos_weight=-1,
+            debug=False),
+        rpn_proposal=dict(
+            nms_pre=2000,
+            max_per_img=2000,
+            nms=dict(type='nms', iou_threshold=0.8),
+            min_bbox_size=0),
+        rcnn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=False,
+                iou_calculator=dict(type='RBboxOverlaps2D'),
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RRandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            pos_weight=-1,
+            debug=False)),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=2000,
+            max_per_img=2000,
+            nms=dict(type='nms', iou_threshold=0.8),
+            min_bbox_size=0),
+        rcnn=dict(
+            nms_pre=2000,
+            min_bbox_size=0,
+            score_thr=0.6,
+            nms=dict(iou_thr=0.1),
+            max_per_img=2000)))
+fp16 = dict(loss_scale=dict(init_scale=512))
+work_dir = 'data/checkpoints/vit-fulltrain'
+auto_resume = False
+gpu_ids = range(0, 1)
