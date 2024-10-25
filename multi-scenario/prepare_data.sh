dd0e69fc7433a6f56d3bf7acf0c623b613fbf6bf
tsdsnk <903257695@qq.com>
2024-10-25 17:03:14 +0800

    update url

--- multi-scenario/prepare_data.sh
+++ multi-scenario/prepare_data.sh
@@ -3,9 +3,9 @@
 
 MULTI_SCENARIO_DIR='./data/'
 
-wget -P $MULTI_SCENARIO_DIR'models/' 'https://github.com/ajksabva/remote-sensing/releases/download/v1.0.0-multi-scenario/mae_pretrain_vit_base_full.pth'
+wget -P $MULTI_SCENARIO_DIR'models/' 'https://github.com/ajksabva/remote-sensing/releases/download/v1.0.0-remote-sensing/mae_pretrain_vit_base_full.pth'
 
-wget -P $MULTI_SCENARIO_DIR'models/' 'https://github.com/ajksabva/remote-sensing/releases/download/v1.0.0-multi-scenario/multi-scenario.pth'
+wget -P $MULTI_SCENARIO_DIR'models/' 'https://github.com/ajksabva/remote-sensing/releases/download/v1.0.0-remote-sensing/multi-scenario.pth'
 
-wget -P $MULTI_SCENARIO_DIR 'https://github.com/ajksabva/remote-sensing/releases/download/v1.0.0-multi-scenario/MASATI-v2.zip' \
+wget -P $MULTI_SCENARIO_DIR 'https://github.com/ajksabva/remote-sensing/releases/download/v1.0.0-remote-sensing/MASATI-v2.zip' \
     && unzip -d $MULTI_SCENARIO_DIR $MULTI_SCENARIO_DIR'MASATI-v2.zip' && rm $MULTI_SCENARIO_DIR'MASATI-v2.zip'
